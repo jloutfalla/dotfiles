ef317c206260a276c8f12d3b9dbe9c4a8932fd93
Jean-Baptiste LOUTFALLA <jb.loutfalla@orange.fr>
2019-09-09 14:37:25 +0200

    Add test for Cirno Day

--- Scripts/.scripts/cirnosay-script.sh
+++ Scripts/.scripts/cirnosay-script.sh
@@ -20,12 +20,19 @@ cirno="Touhoudex_Chibi_Cirno.png Touhoudex_Cirno.png" #Touhoudex_EX_Cirno.png
 nbrRandom=$(random.sh)
 randomCirno=$(((nbrRandom % 2) + 1))
 
-quotes="I'm the strongest !|You BAKA !|Yes eye can !"
-nbrRandom=$(random.sh)
-randomQuotes=$(((nbrRandom % 3) + 1))
+
+if [ "$(date +%m)" -eq 9 ] && [ "$(date +%d)" -eq 9 ]
+then
+	quotes="Happy Cirno Day !"
+else
+	quotes="I'm the strongest !|You BAKA !|Yes eye can !"
+	nbrRandom=$(random.sh)
+	randomQuotes=$(((nbrRandom % 3) + 1))
+
+	quotes=$(echo "$quotes" | cut -d "|" -f "$randomQuotes")
+fi
 
 cirno=$(echo "$cirno" | cut -d " " -f "$randomCirno")
-quotes=$(echo "$quotes" | cut -d "|" -f "$randomQuotes")
 
 echo ""
 cirnosay -b a -i "$CIRNO_IMAGES_PATH/$cirno" "$quotes"
