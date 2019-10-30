760464e89ed7176698fe3afb1e39c3da8fa8cf7d
Loutfalla Jean-Baptiste <jb.loutfalla@orange.fr>
2019-10-30 01:56:07 +0100

    Change theme and add stuff

--- Emacs/.emacs.d/init.el
+++ Emacs/.emacs.d/init.el
@@ -6,9 +6,9 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq package-archives '(("ELPA"  . "http://tromey.com/elpa/")
-						 ("gnu"   . "http://elpa.gnu.org/packages/")
-						 ("melpa" . "https://melpa.org/packages/")
-						 ("org"   . "https://orgmode.org/elpa/")))
+			 ("gnu"   . "http://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")
+			 ("org"   . "https://orgmode.org/elpa/")))
 (package-initialize)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -31,17 +31,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (spacemacs-dark)))
- '(custom-safe-themes
-   (quote
-	("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(ansi-color-names-vector
+   ["#282c34" "#ff6c6b" "#98be65" "#da8548" "#61afef" "#c678dd" "#1f5582" "#abb2bf"])
  '(package-selected-packages
    (quote
-	(rainbow-mode which-key use-package spacemacs-theme flycheck))))
+    (company-php php-mode company-shell slime-company slime racer cargo company-racer flycheck-rust rust-mode company-irony company-c-headers flycheck-clang-analyzer company flycheck yasnippet-snippets expand-region linum-relative rainbow-delimiters rainbow-mode which-key async zerodark-theme use-package))))
+;;;:family "Bitstream Vera Sans Mono"
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 155 :width normal :foundry "PfEd" :family "xos4 Terminus")))))
-;;;:family "Bitstream Vera Sans Mono"
