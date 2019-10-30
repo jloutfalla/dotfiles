731cb3a48b9c2fe0b768e38fb51ce460d732a22b
Loutfalla Jean-Baptiste <jb.loutfalla@orange.fr>
2019-10-30 02:34:20 +0100

    Faster Emacs start up and linum-relatives backends

--- Emacs/.emacs.d/init.el
+++ Emacs/.emacs.d/init.el
@@ -1,6 +1,23 @@
-;;; This is all kind of necessary
+;;; This fixed garbage collection, makes emacs start up faster ;;;;;;;
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
+(defvar startup/file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(defun startup/revert-file-name-handler-alist ()
+  (setq file-name-handler-alist startup/file-name-handler-alist))
+
+(defun startup/reset-gc ()
+  (setq gc-cons-threshold 16777216
+	gc-cons-percentage 0.1))
+
+(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
+(add-hook 'emacs-startup-hook 'startup/reset-gc)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; This is all kind of necessary
 
 ;;; Activating the melpa depot
 (require 'package)
