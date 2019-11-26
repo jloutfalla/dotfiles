d98da6c1eb1fe089140afa0e4abc9e5200bee631
Loutfalla Jean-Baptiste <jb.loutfalla@orange.fr>
2019-11-27 00:39:38 +0100

    Add spellchecker + correct mistakes

--- Emacs/.emacs.d/init.el
+++ Emacs/.emacs.d/init.el
@@ -52,7 +52,7 @@
    ["#282c34" "#ff6c6b" "#98be65" "#da8548" "#61afef" "#c678dd" "#1f5582" "#abb2bf"])
  '(package-selected-packages
    (quote
-    (company-php php-mode company-shell slime-company slime racer cargo company-racer flycheck-rust rust-mode company-irony company-c-headers flycheck-clang-analyzer company flycheck yasnippet-snippets expand-region linum-relative rainbow-delimiters rainbow-mode which-key async zerodark-theme use-package))))
+    (flycheck-grammalecte company-php php-mode company-shell slime-company slime racer cargo company-racer flycheck-rust rust-mode company-irony company-c-headers flycheck-clang-analyzer company flycheck yasnippet-snippets expand-region linum-relative rainbow-delimiters rainbow-mode which-key async zerodark-theme use-package))))
 ;;;:family "Bitstream Vera Sans Mono"
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
