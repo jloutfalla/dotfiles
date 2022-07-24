421a18c0004acef4ec9fc30340aff77d27312e2f
Jean-Baptiste Loutfalla <jb.loutfalla@orange.fr>
2022-07-24 17:36:19 +0200

    Update Emacs config files to be a bit cleaner
    - Change the init.el and README.org files to use a proper namespace
    for variables and functions.
    - Update init.el to be able to tangle README.org if config.el is not
    present.

--- Emacs/.config/emacs/init.el
+++ Emacs/.config/emacs/init.el
@@ -1,16 +1,18 @@
-(setq config-file
-      (expand-file-name (concat (getenv "XDG_CONFIG_HOME")
-                                "/emacs/config.el"))
-      custom-file
-      (expand-file-name (concat (getenv "XDG_CONFIG_HOME")
-                                "/emacs/custom.el")))
+(defconst jl/config-file (expand-file-name (concat (getenv "XDG_CONFIG_HOME")
+						   "/emacs/config.el")))
+(defconst jl/config-file-org (expand-file-name (concat (getenv "XDG_CONFIG_HOME")
+                                                       "/emacs/README.org")))
 
-(setq gc-cons-threshold (* 50 1000 1000))
+(setq custom-file (expand-file-name (concat (getenv "XDG_CONFIG_HOME")
+					    "/emacs/custom.el"))
+      gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024))
 
-(when (file-readable-p config-file)
-  (load config-file))
-
-(setq gc-cons-threshold (* 2 1000 1000))
+(cond ((file-readable-p jl/config-file) (load jl/config-file))
+      ((file-readable-p jl/config-file-org)
+       (require 'org)
+       (org-babel-tangle-file jl/config-file-org)
+       (load jl/config-file)))
 
 (when (file-readable-p custom-file)
   (load custom-file))
